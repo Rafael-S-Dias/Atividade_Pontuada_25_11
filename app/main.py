39d4036972c30c5208329b42745a73aa120c86bd
Rafael-S-Dias <161033176+Rafael-S-Dias@users.noreply.github.com>
2024-11-25 01:06:04 +0000

    Correção de Bugs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from services.usuario_service import UsuarioService
 from repositories.usuario_repository import UsuarioRepository
 from config.database import Session
@@ -11,39 +12,53 @@ def main():
     service = UsuarioService(repository)
 
     def menu():
-        print("======== SENAI SOLUTION =========")
-        print("BEM VINDO!\n")
-        print("1 - Adicionar usuário \n2 - Pesquisar um usuário \n3 - Atualizar dados de um usuário \
-            \n4 - Excluir um usuário \n5 - Exibir todos os usuários cadastrados \n0 - Sair")
-        
-        try:
-            opcao = int(input("\nSelecione a opção desejada: "))
-        except ValueError:
-            print("Comando inválido! Por favor, insira um número inteiro.")
-            return 
+        while True:
+            print("======== SENAI SOLUTION =========")
+            print("BEM VINDO!\n")
+            print("1 - Adicionar usuário \n2 - Pesquisar um usuário \n3 - Atualizar dados de um usuário \
+                \n4 - Excluir um usuário \n5 - Exibir todos os usuários cadastrados \n0 - Sair")
+            
+            try:
+                opcao = int(input("\nSelecione a opção desejada: "))
+            except ValueError:
+                print("Comando inválido! Por favor, insira um número inteiro.")
+                return 
 
-        match opcao:
-            case 1:
-                return service.criar_usuario()
-            case 2:
-                return service.pesquisar_usuario()
-            case 3:
-                return service.atualizar_usuario()
-            case 4: 
-                return service.deletar_usuario()
-            case 5: 
-                print("\nListando todos os usuários: ")
-                usuarios = service.listar_todos_usuarios()
+            match opcao:
+                case 1:
+                    os.system("cls || clear")
+                    service.criar_usuario()
+                    time.sleep(2)
+                case 2:
+                    os.system("cls || clear")
+                    service.pesquisar_usuario()
+                    time.sleep(2)
+                case 3:
+                    os.system("cls || clear")
+                    service.atualizar_usuario()
+                    time.sleep(2)
+                case 4: 
+                    os.system("cls || clear")
+                    service.deletar_usuario()
+                    time.sleep(2)
+                case 5: 
+                    os.system("cls || clear")
+                    print("\nListando todos os usuários: ")
+                    usuarios = service.listar_todos_usuarios()
 
-                for usuario in usuarios:
-                    print(f"{usuario.id} - {usuario.nome} - {usuario.email}")
-                return  
-            case 0:
-                print("Encerrando programa\n")
-                print("PROGRAMA FINALIZADO!")
-                return  
-            case _:
-                print("Opção inválida! Por favor, selecione uma opção válida.")
+                    for usuario in usuarios:
+                        print(f"{usuario.id} - {usuario.nome} - {usuario.email}") 
+                        time.sleep(2)
+                case 0:
+                    os.system("cls || clear")
+                    print("Encerrando programa...")
+                    time.sleep(2)
+                    print("PROGRAMA FINALIZADO!")
+                    return  
+                case _:
+                    os.system("cls || clear")
+                    print("Opção inválida! Por favor, selecione uma opção válida.")
+                    time.sleep(2)
 
 
     menu()
